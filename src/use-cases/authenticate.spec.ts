8229c0f307d29ebddd8770ce825875292412ec47
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-05-06 18:00:00 -0300

    feat: profile use case

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,7 +1,7 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
-import { InvalidCredentialsError } from './errors/invalid-credentials'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { AuthenticateUseCase } from './authenticate'
 
 let usersRepository: InMemoryUsersRepository
